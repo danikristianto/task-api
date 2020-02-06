5b196ba9a6aa8300da3566d0f49631d9e5c8c6bc
Dani Kristianto <danikristianto21@gmail.com>
2020-02-06 13:10:34 +0700

    initial commit

---
+++ src/emails/accounts.js
@@ -0,0 +1,26 @@
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'dendani3al@gmail.com',
+        subject: 'Thanks for joining in!',
+        text: `Hi ${name}, Welcome to our site`
+    })
+}
+
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'dendani3al@gmail.com',
+        subject: 'Sorry to see you go!',
+        text: `GoodBye ${name}, I hope to see you back sometime soon.`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail, 
+    sendCancelationEmail
+}
